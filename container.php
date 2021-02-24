02109f8673743c0158d9e4a8b42d1ae0c6300c80
koujali <kouj0304@google.com>
2021-02-24 09:11:15 +0400

    initia

---
+++ container.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Bootstrap</title>
+  </head>
+  <body class="ml-5">
+    <br>
+    <div class="container">
+        <div class="row test">
+            <div class="col-xs-10 col-md-4">
+                    <div class="card bg-light p-1" style="height:20rem">
+                    <img class="card-img-top" src="beach.jpeg" alt="Image">
+                        <div class="card-body">
+                            <div class="card-title">Title:</div>
+                                <div class="card-text">Description of the image</div>
+                        </div>
+                    </div>
+            </div>
+        
+            <div class="col-xs-12 col-md-4">
+                <div class="card bg-muted" style="height:20rem">Example 2</div>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <div class="card bg-danger" style="height:20rem">Example 3</div>
+            </div>
+        </div>
+    </div>
+    <div class="container mb-3">
+        <div class="row">
+        
+        
+            <div class="col-xs-12 col-12 d-flex mt-4">
+                <div class="card bg-muted mr-5" style="width:100%; height:20rem">Example 1</div> 
+                <div class="card bg-muted mr-5" style="width:100%; height:20rem">Example 2</div>
+                <div class="card bg-muted" style="width:100%; height:20rem">Example 3</div>
+            </div>           
+        </div>
+    </div>
+<form>
+    <div class="row mb-2 ml-3">
+        <div class="col-2 d-flex mr-2">
+            <input placeholder="Please enter your name" type="text" class="mr-4">
+            <input placeholder="Please enter your email" type="text">
+        </div>
+    </div>
+    <div class="row">
+        <button type="button" class="btn btn-outline-success">Submit</button>
+    </div>
+</form>
+
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+    <label class="btn btn-success">
+        <input type="radio" autocomplete="off">Radio 1</div>
+    </label>
+    <label class="btn btn-secondary active">
+        <input type="radio" name="options" id="option1" autocomplete="off">Active
+    </label>
+</div>
+
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+  </label>
+</div>
+</body>
+</html>
+  
+  
+  </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ form.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html>
+<head>
+<title>Login Form</title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="log_form">Open</button>
+
+<div class="modal fade"></div>
+
+
+
+</body>
+</html>
